1feb031f9d3a518c9b4f02b53dced0eae9a92797
ymt <t_001ing@163.com>
2021-08-06 20:40:42 +0800

    上传day08_09、day10_11、day12_14

---
+++ day10_11/src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header
+        style="
+          margin-bottom: 30px;
+          box-shadow: 0 3px 10px #e0eaf2;
+          background: #fff;
+        "
+      >
+        <div class="head">
+          <div class="head_left">
+            <img src="@/assets/timing.png" alt="" />
+            <p>猩猩点灯</p>
+          </div>
+          <div class="head_right">
+            <img class="right_img1" src="@/assets/bell.png" alt="" />
+            <span>12</span>
+            <img src="@/assets/pic.png" alt="" />
+          </div>
+        </div>
+      </el-header>
+      <el-container style="padding: 0 100px">
+        <el-aside
+          width="200px"
+          style="margin-right: 30px; box-shadow: 0 2px 12px 0 #e0eaf2"
+        >
+          <Aside />
+        </el-aside>
+        <el-main style="background: skyblue; padding: 0">
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Aside from "@/components/Aside.vue";
+
+export default {
+  components: {
+    Aside,
+  },
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+  height: 100%;
+  background: #f7f9fa;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.head {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.head_left {
+  display: flex;
+  align-items: center;
+}
+
+.head_left p {
+  color: #0dd89e;
+  margin-left: 10px;
+  font-weight: bold;
+  font-size: 24px;
+  font-family: "Microsoft YaHei";
+}
+
+.head_right {
+  display: flex;
+  align-items: center;
+}
+
+.head_right img:first-of-type {
+  width: 30px;
+  height: 30px;
+  margin-right: 5px;
+}
+
+.head_right img:last-of-type {
+  border-radius: 50%;
+}
+
+.head_right span {
+  font-size: 12px;
+  color: #fff;
+  background: #f56c6c;
+  width: 35px;
+  border-radius: 10px;
+  position: relative;
+  right: 20px;
+  bottom: 12px;
+}
+
+img {
+  width: 35px;
+  height: 35px;
+}
+</style>
---
+++ day10_11/src/components/Aside.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="aside">
+    <el-row class="tac">
+      <el-col :span="12">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <router-link :to="{ name: 'main' }">
+            <el-menu-item index="1">
+              <i class="el-icon-s-shop"></i>
+              <span slot="title">主页</span>
+            </el-menu-item>
+          </router-link>
+
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>管理</span>
+            </template>
+            <el-menu-item index="2-1">内容管理</el-menu-item>
+            <el-menu-item index="2-2">个人主页管理</el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-s-data"></i>
+              <span>涨粉</span>
+            </template>
+            <el-menu-item index="3-1">赞助自习室</el-menu-item>
+            <el-menu-item index="3-2">答疑</el-menu-item>
+            <el-menu-item index="3-3">举办live</el-menu-item>
+          </el-submenu>
+
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-wallet"></i>
+              <span>创收</span>
+            </template>
+            <el-menu-item index="4-1">发布专辑</el-menu-item>
+            <el-menu-item index="4-2">举办付费Live</el-menu-item>
+          </el-submenu>
+
+          <el-menu-item index="5">
+            <i class="el-icon-s-finance"></i>
+            <span slot="title">我的资产</span>
+          </el-menu-item>
+
+          <el-menu-item index="6">
+            <i class="el-icon-upload"></i>
+            <span slot="title">我的网盘</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.aside {
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+}
+
+.aside >>> .el-row .el-menu-vertical-demo {
+  padding-bottom: 50px;
+}
+
+.aside >>> .el-row .el-menu-vertical-demo li {
+  text-align: left;
+}
+
+.aside >>> .el-col-12 {
+  width: 100%;
+  height: 100%;
+}
+</style>
